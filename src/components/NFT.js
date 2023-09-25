c696d908c4c7d6774179ac90901442a73801e907
SnowyField <aswdqe1x@gmail.com>
2023-09-25 19:27:04 +0700

    deploy ghpages

--- src/components/NFT.js
+++ src/components/NFT.js
@@ -12,34 +12,40 @@ function NFT({ userId, nftId, nftName, price, thumbnail, listing, isRootStock, p
     }, [userId])
 
     return (
-        <div className="px-3 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+        <div className="px-3 w-[23rem] h-[30rem]">
             <div className="bg-white overflow-hidden rounded-xl text-gray-500">
-                <a href="#" className="block relative object-cover">
-                    <img src={thumbnail} className="hover:opacity-90 h-96 w-96 object-cover" alt="..." />
-                    {!privateMeta &&
-                        <div className="group absolute bg-gray-900 bottom-4 gap-2 inline-flex items-center opacity-75 right-6 rounded-full text-white px-3 h-10 text-lg">
+                <a href="#" className="block relative object-cover w-[23rem] h-[23rem]">
+                    <img
+                        src={thumbnail}
+                        className="hover:opacity-90 h-full w-full object-cover"
+                        alt="..."
+                        style={{ objectFit: 'cover', backgroundSize: 'cover', backgroundClip: 'cover' }}
+                    />
+                    {!privateMeta && (
+                        <div className="group absolute bg-gray-900 bottom-4 right-10 gap-2 inline-flex items-center opacity-75 rounded-full text-white px-3 h-10 text-lg">
                             <span className="group-hover:block hidden text-sm">Prompt is available</span>
                             <FaRegFaceSmileWink />
                         </div>
-                    }
+                    )}
                 </a>
-                <div className="px-4 py-6 sm:px-6">
-                    <div className="flex items-center justify-between">
+
+                <div className="px-4 my-auto sm:px-6 h-[8rem] grid">
+                    <div className="flex items-center justify-between self-center h-[3.5rem]">
                         <h3 className="font-bold text-xl">
                             <a href="#" className="hover:text-primary-500 text-gray-900">{nftName}</a>
                         </h3>
                         <a className="hover:text-primary-500 inline-block rounded-full text-gray-900" href="#" aria-label="add to favorite">
                             {
                                 isRootStock ?
-                                    <img src='./assets/rootstock_logo.png' alt="..." width="42" height="42" />
-                                    : <img src='./assets/ordinals_logo.png' alt="..." width="36" height="36" />
+                                    <img src='/assets/rootstock_logo.png' alt="..." width="42" height="42" />
+                                    : <img src='/assets/ordinals_logo.png' alt="..." width="36" height="36" />
                             }
                         </a>
                     </div>
-                    <hr className="border-gray-200 my-4" />
-                    <div className="flex flex-col">
-                        <div className='flex items-center justify-between'>
-                            <div className='group items-start'>
+                    <hr className="border-gray-200 h-[0.5rem]" />
+                    <div className="flex flex-col items-center h-[4rem] place-content-center ">
+                        <div className='flex items-center justify-between w-full'>
+                            <div className='group'>
                                 <Link to={"/profile/" + user.id} className="group-hover:text-primary-500 inline-flex italic items-center space-x-2 text-sm">
                                     <img src={user.picture} className="border-2 group-hover:border-primary-500 border-white rounded-full" alt="..." width="36" height="36" />
                                     <span>{user.name}</span>
